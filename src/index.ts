d89664c079808a5886b67f9471b40b0785a20db7
kimlyNhep <kimly.nhep0168@gmail.com>
2021-06-03 22:39:12 +0700

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const app = async () => {
   const app = express();
 
   app.use(cookieParser());
-  app.use(cors({ origin: '*', credentials: true }));
+  app.use(cors({ origin: 'https://movie-academy-front.vercel.app/', credentials: true }));
   app.use(
     graphqlUploadExpress({
       maxFileSize: 10000000,
