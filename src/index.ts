e9bd2f6cf1d1997c2ee605a76db7627f5ae4388e
kimlyNhep <kimly.nhep0168@gmail.com>
2021-06-29 21:48:14 +0700

    done

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,10 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-// import { accessToken } from "./token";
-// import { User } from "./entity/User";
-// import { verify } from "jsonwebtoken";
 import { MovieContext } from './MovieContext';
 import { createConnection, getConnectionOptions } from 'typeorm';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-// import { sendRefreshToken } from "./sendRefreshToken";
 import { graphqlUploadExpress } from 'graphql-upload';
 import { uploadResolver } from './resolver/uploadResolvers';
 import { genreResolvers } from './resolver/genreResolvers';
@@ -56,25 +52,6 @@ const app = async () => {
   app.use('/images', express.static('images'));
   app.get('/', (_req, res) => res.send('Hello'));
 
-  // app.post("/refresh_token", async (req, res) => {
-  //   const token = req.cookies.token;
-  //   if (!token) return res.send({ ok: false, accessToken: "" });
-
-  //   let payload: any = null;
-  //   try {
-  //     payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
-  //   } catch (err) {
-  //     return res.send({ ok: false, accessToken: "" });
-  //   }
-
-  //   const user = await User.findOne({ id: payload.userId });
-  //   if (!user) return res.send({ ok: false, accessToken: "" });
-
-  //   sendRefreshToken(res, accessToken(user));
-
-  //   return res.send({ ok: true, accessToken: accessToken(user) });
-  // });
-
   try {
     let connectionOptions = await getConnectionOptions();
 
@@ -84,27 +61,6 @@ const app = async () => {
     connectionOptions = await getConnectionOptions('default');
 
     await createConnection(connectionOptions);
-
-    // await createConnection({
-    //   type: 'postgres',
-    //   host: 'ec2-54-205-183-19.compute-1.amazonaws.com',
-    //   port: 5432,
-    //   username: 'joytkawnlpwdcq',
-    //   password:
-    //     '546c039124795af20e024347182ea9b8b280a28bf281714bae1fc2b42748b6ee',
-    //   database: 'ddrgs892vhn6ak',
-    //   synchronize: false,
-    //   logging: false,
-    //   entities: ['dist/entity/**/*.js'],
-    //   migrations: ['dist/migration/**/*.js'],
-    //   subscribers: ['dist/subscriber/**/*.js'],
-    //   ssl: true,
-    //   extra: {
-    //     ssl: {
-    //       rejectUnauthorized: false,
-    //     },
-    //   },
-    // });
 
     const apolloServer = new ApolloServer({
       introspection: true,
